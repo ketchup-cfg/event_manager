a98122094052139a53e61335c15c349b82436b69
Trevor Pierce <yeahtrevorpierce@gmail.com>
2024-04-01 15:06:48 -0500

    Add phone number parser

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -8,6 +8,18 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
+def clean_phone_number(phone_number)
+  phone_number.gsub!(/[^\d]/, '')
+
+  if phone_number.length == 10
+    phone_number
+  elsif phone_number.length == 11 && phone_number[0] == '1'
+    phone_number[1..]
+  else
+    'bad input'
+  end
+end
+
 def build_civic_info
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = ENV['API_KEY']
@@ -51,6 +63,8 @@ contents.each do |row|
   id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
+  phone = clean_phone_number(row[:homephone])
+  puts "#{name} #{phone}"
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
